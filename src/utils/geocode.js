2630db9812e351aec19dc815c2f1cbc715b34c22
MacBook Air <macbookair@macbook-air-de-macbook.home>
2022-03-22 19:24:08 +0100

    Init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('postman-request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiZXhjZTE3MDkiLCJhIjoiY2t6cHliaWlzMDJ0ajJvbzlkOWMzNHI4eCJ9.w7Jq0cBSK30r-FMpCui4Yg&limit=1'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
